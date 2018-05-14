6b4826ef1a7d0458c6b237526b674afbd0f4e57d
Shenghan Gao <gaoshenghan199123@gmail.com>
2018-05-13 22:39:18 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+
+const modules = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  FormsModule,
+  HttpModule,
+  MatToolbarModule,
+  FlexLayoutModule,
+];
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: modules,
+  exports: modules,
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
